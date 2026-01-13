b9b5d072cc4691417923576b4fcfa128796662db
jdaniel3331 <jdaniel10031@outlook.com>
2026-01-12 21:58:21 -0600

    feat: First commit

---
+++ src/main/java/com/jdaniel/login_with_jwt_good/LoginWithJwtGoodApplication.java
@@ -0,0 +1,13 @@
+package com.jdaniel.login_with_jwt_good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithJwtGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithJwtGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdaniel/login_with_jwt_good/LoginWithJwtGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdaniel.login_with_jwt_good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithJwtGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
